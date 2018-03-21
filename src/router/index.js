9d7f14fc9d5c9ccbdf02e2ac79e193c49e0e8bc6
Mohammed Nouayti <med.nouayti@gmail.com>
2018-03-21 12:26:20 +0000

    vue-cli webpack, routes and some components

---
+++ src/router/index.js
@@ -0,0 +1,54 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import Home from '@/components/pages/Home';
+import Cart from '@/components/pages/Cart';
+
+// Admin Components
+import Index from '@/components/pages/admin/Index'
+import New from '@/components/pages/admin/New'
+import Products from '@/components/pages/admin/Products'
+import Edit from '@/components/pages/admin/Edit';
+
+Vue.use(Router);
+
+export default new  Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/cart',
+      name: 'Cart',
+      component: Cart
+    },
+    {
+      path: '/admin',
+      name: 'Admin',
+
+      // Parent routes still has a component
+      component: Index,
+
+      // Child routes
+      children: [
+        {
+          path: 'new',
+          name: 'New',
+          component: New
+        },
+        {
+          path: '',
+          name: 'Products',
+          component: Products
+        },
+        {
+          path: 'edit/:id',
+          name: 'Edit',
+          component: Edit
+        }
+      ]
+    }
+  ]
+});
